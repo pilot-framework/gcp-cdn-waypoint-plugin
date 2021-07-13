dea30466b214b7f57d73385c2a31d694dfa7aa07
grahamhub <grammar@hey.com>
2021-07-13 15:55:47 -0500

    feat: destroy provisioned bucket

--- platform/destroy.go
+++ platform/destroy.go
@@ -2,7 +2,10 @@ package platform
 
 import (
 	"context"
+	"fmt"
 
+	"cloud.google.com/go/storage"
+	"google.golang.org/api/iterator"
 	"github.com/hashicorp/waypoint-plugin-sdk/terminal"
 )
 
@@ -38,6 +41,45 @@ func (p *Platform) DestroyFunc() interface{} {
 //
 // If an error is returned, Waypoint stops the execution flow and
 // returns an error to the user.
-func (p *Platform) destroy(ctx context.Context, ui terminal.UI, deployment *Deployment) error {
+func (p *Platform) destroy(ctx context.Context, ui terminal.UI) error {
+	u := ui.Status()
+	defer u.Close()
+	u.Step("", "---Destroying Cloud Storage Assets---")
+
+	client, err := storage.NewClient(ctx)
+	if err != nil {
+		return fmt.Errorf("failed to connect to Google Cloud: %s", err.Error())
+	}
+	defer client.Close()
+
+	u.Update("Destroying objects...")
+
+	it := client.Bucket(p.config.Bucket).Objects(ctx, nil)
+	for {
+		attrs, err := it.Next()
+		if err == iterator.Done {
+			break
+		}
+		if err != nil {
+			return fmt.Errorf("failed to list object: %s", err.Error())
+		}
+
+		obj := client.Bucket(p.config.Bucket).Object(attrs.Name)
+		if err := obj.Delete(ctx); err != nil {
+			return fmt.Errorf("failed to destroy object: %s", err.Error())
+		}
+	}
+
+	u.Step("", "Destroyed objects")
+
+	u.Update("Destroying bucket...")
+
+	bkt := client.Bucket(p.config.Bucket)
+	if err := bkt.Delete(ctx); err != nil {
+		return fmt.Errorf("failed to destroy bucket: %s", err.Error())
+	}
+
+	u.Step(terminal.StatusOK, "Successfully destroyed Cloud Storage Assets")
+
 	return nil
 }
