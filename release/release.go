d91abc1955590c97c909c72f56f19e09c6ff3439
grahamhub <grammar@hey.com>
2021-07-13 11:23:31 -0500

    fix: attach gcloud fns to struct

--- release/release.go
+++ release/release.go
@@ -12,6 +12,59 @@ import (
 	"github.com/pilot-framework/gcp-cdn-waypoint-plugin/platform"
 )
 
+type GCloud struct {
+	Project string
+	Bucket string
+}
+
+func (g *GCloud) Exec(args []string) (string, error) {
+	var stdout, stderr bytes.Buffer
+	cmd := exec.Command("gcloud", args...)
+	cmd.Stdout = &stdout
+	cmd.Stderr = &stderr
+	err := cmd.Run()
+	if err != nil {
+		return "", errors.New(string(bytes.TrimSpace(stderr.Bytes())))
+	}
+	return string(bytes.TrimSpace(stdout.Bytes())), nil
+}
+
+func (g *GCloud) ListIPAddresses() (string, error) {
+	return g.Exec([]string{
+		"compute", "addresses", "list", "--project="+g.Project,
+	})
+}
+
+func (g *GCloud) GetStaticIP() (string, error) {
+	return g.Exec([]string{
+		"compute", "addresses", "describe", g.Bucket+"-ip", "--format=get(address)", "--global", "--project="+g.Project,
+	})
+}
+
+func (g * GCloud) ReserveIPAddress() (string, error) {
+	return g.Exec([]string{
+		"compute", "addresses", "create", g.Bucket+"-ip", "--network-tier=PREMIUM", "--ip-version=IPV4", "--global", "--project="+g.Project,
+	})
+} 
+
+func (g *GCloud) CreateBackendBucket() (string, error) {
+	return g.Exec([]string{
+		"compute", "backend-buckets", g.Bucket+"-backend-bucket", "--gcs-bucket-name="+"b", "--enable-cdn", "--project="+g.Project,
+	})
+}
+
+func (g *GCloud) CreateURLMap() (string, error) {
+	return g.Exec([]string{
+		"compute", "url-maps", "create", g.Bucket+"-lb", "--default-backend-bucket="+g.Bucket+"-backend-bucket", "--project="+g.Project,
+	})
+}
+
+func (g *GCloud) CreateSSLCert(domain string) (string, error) {
+	return g.Exec([]string{
+		"compute", "ssl-certificates", "create", g.Bucket+"-cert", "--domains="+domain, "--global", "--project="+g.Project,
+	})
+}
+
 type ReleaseConfig struct {
 	Domain string `hcl:"domain"`
 }
@@ -52,7 +105,9 @@ func (rm *ReleaseManager) release(ctx context.Context, ui terminal.UI, target *p
 	defer u.Close()
 	u.Step("", "---Releasing to Cloud CDN---")
 
-	out, err := ListIPAddresses(target.Project)
+	gc := &GCloud{Bucket: target.Bucket, Project: target.Project}
+
+	out, err := gc.ListIPAddresses()
 	if err != nil {
 		return nil, fmt.Errorf("failed to get listing of external IPs: %s", err.Error())
 	}
@@ -61,14 +116,14 @@ func (rm *ReleaseManager) release(ctx context.Context, ui terminal.UI, target *p
 		u.Step(terminal.StatusOK, "Found existing external IP address")
 	} else {
 		u.Step("", "Reserving external IP address")
-		_, err := ReserveIPAddress(target.Bucket, target.Project)
+		_, err := gc.ReserveIPAddress()
 		if err != nil {
 			return nil, fmt.Errorf("failed to get listing of external IPs: %s", err.Error())
 		}
 		u.Step(terminal.StatusOK, "External IP reserved")
 	}
 
-	ipAddr, err := GetStaticIP(target.Bucket, target.Project)
+	ipAddr, err := gc.GetStaticIP()
 	if err != nil {
 		return nil, fmt.Errorf("failed to get external IP: %s", err.Error())
 	}
@@ -77,78 +132,6 @@ func (rm *ReleaseManager) release(ctx context.Context, ui terminal.UI, target *p
 	u.Step("", "ipaddress="+ipAddr)
 
 	return &Release{}, nil
-}
-
-func GetStaticIP(b, projID string) (string, error) {
-	var stdout, stderr bytes.Buffer
-	cmd := exec.Command("gcloud", "compute", "addresses", "describe", b+"-ip", "--format=get(address)", "--global", "--project="+projID)
-	cmd.Stdout = &stdout
-	cmd.Stderr = &stderr
-	err := cmd.Run()
-	if err != nil {
-		return "", errors.New(string(bytes.TrimSpace(stderr.Bytes())))
-	}
-	return string(bytes.TrimSpace(stdout.Bytes())), nil
-}
-
-func ListIPAddresses(projID string) (string, error) {
-	var stdout, stderr bytes.Buffer
-	cmd := exec.Command("gcloud", "compute", "addresses", "list", "--project="+projID)
-	cmd.Stdout = &stdout
-	cmd.Stderr = &stderr
-	err := cmd.Run()
-	if err != nil {
-		return "", errors.New(string(bytes.TrimSpace(stderr.Bytes())))
-	}
-	return string(bytes.TrimSpace(stdout.Bytes())), nil
-}
-
-func ReserveIPAddress(b, projID string) (string, error) {
-	var stdout, stderr bytes.Buffer
-	cmd := exec.Command("gcloud", "compute", "addresses", "create", b+"-ip", "--network-tier=PREMIUM", "--ip-version=IPV4", "--global", "--project="+projID)
-	cmd.Stdout = &stdout
-	cmd.Stderr = &stderr
-	err := cmd.Run()
-	if err != nil {
-		return "", errors.New(string(bytes.TrimSpace(stderr.Bytes())))
-	}
-	return string(bytes.TrimSpace(stdout.Bytes())), nil
-}
-
-func CreateBackendBucket(b, projID string) (string, error) {
-	var stdout, stderr bytes.Buffer
-	cmd := exec.Command("gcloud", "compute", "backend-buckets", b+"-backend-bucket", "--gcs-bucket-name="+"b", "--enable-cdn", "--project="+projID)
-	cmd.Stdout = &stdout
-	cmd.Stderr = &stderr
-	err := cmd.Run()
-	if err != nil {
-		return "", errors.New(string(bytes.TrimSpace(stderr.Bytes())))
-	}
-	return string(bytes.TrimSpace(stdout.Bytes())), nil
-}
-
-func CreateURLMap(b, projID string) (string, error) {
-	var stdout, stderr bytes.Buffer
-	cmd := exec.Command("gcloud", "compute", "url-maps", "create", b+"-lb", "--default-backend-bucket="+b+"-backend-bucket", "--project="+projID)
-	cmd.Stdout = &stdout
-	cmd.Stderr = &stderr
-	err := cmd.Run()
-	if err != nil {
-		return "", errors.New(string(bytes.TrimSpace(stderr.Bytes())))
-	}
-	return string(bytes.TrimSpace(stdout.Bytes())), nil
-}
-
-func CreateSSLCert(b, projID, domain string) (string, error) {
-	var stdout, stderr bytes.Buffer
-	cmd := exec.Command("gcloud", "compute", "ssl-certificates", "create", b+"-cert", "--domains="+domain, "--global", "--project="+projID)
-	cmd.Stdout = &stdout
-	cmd.Stderr = &stderr
-	err := cmd.Run()
-	if err != nil {
-		return "", errors.New(string(bytes.TrimSpace(stderr.Bytes())))
-	}
-	return string(bytes.TrimSpace(stdout.Bytes())), nil
 }
 
 //TODO: configure target https proxy
