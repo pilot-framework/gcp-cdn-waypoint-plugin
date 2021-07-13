4e8d19ed176ed238422b7c2c65776e99b407e9d9
grahamhub <grammar@hey.com>
2021-07-13 13:55:51 -0500

    fix: rewrite release to use gcloud pkg

--- release/release.go
+++ release/release.go
@@ -52,33 +52,95 @@ func (rm *ReleaseManager) release(ctx context.Context, ui terminal.UI, target *p
 
 	gc := gcloud.Init(target.Project, target.Bucket)
 
-	out, err := gc.IP.List()
-	if err != nil {
-		return nil, fmt.Errorf("failed to get listing of external IPs: %s", err.Error())
-	}
+	// PROVISION IP ADDRESS
+	u.Update("Configuring IP Address...")
 
-	if strings.Contains(out, target.Bucket) {
-		u.Step(terminal.StatusOK, "Found existing external IP address")
+	if gc.IP.Exists() {
+		u.Step(terminal.StatusOK, "Found existing external IP Address")
 	} else {
-		u.Step("", "Reserving external IP address")
+		u.Update("Reserving new external IP Address...")
 		_, err := gc.IP.Reserve()
 		if err != nil {
-			return nil, fmt.Errorf("failed to get listing of external IPs: %s", err.Error())
+			return nil, fmt.Errorf("failed to reserve IP Address: %s", err.Error())
 		}
-		u.Step(terminal.StatusOK, "External IP reserved")
+
+		u.Step(terminal.StatusOK, "Reserved new external IP Address")
 	}
 
-	ipAddr, err := gc.IP.GetStatic()
-	if err != nil {
-		return nil, fmt.Errorf("failed to get external IP: %s", err.Error())
+	// PROVISION BACKEND BUCKET
+	u.Update("Configuring backend bucket...")
+
+	if gc.BackendBucket.Exists() {
+		u.Step(terminal.StatusOK, "Found existing backend bucket")
+	} else {
+		u.Update("Creating new backend bucket...")
+		_, err := gc.BackendBucket.Create()
+		if err != nil {
+			return nil, fmt.Errorf("failed to create backend bucket: %s", err.Error())
+		}
+
+		u.Step(terminal.StatusOK, "Created new backend bucket")
 	}
 
-	//TODO: load balance configuration
-	u.Step("", "ipaddress="+ipAddr)
+	// PROVISION LOAD BALANCER
+	u.Update("Configuring load balancer...")
+
+	if gc.URLMap.Exists() {
+		u.Step(terminal.StatusOK, "Found existing load balancer")
+	} else {
+		u.Update("Creating new load balancer...")
+		_, err := gc.URLMap.Create()
+		if err != nil {
+			return nil, fmt.Errorf("failed to create load balancer: %s", err.Error())
+		}
+
+		u.Step(terminal.StatusOK, "Created new load balancer")
+	}
+
+	// GENERATE SSL CERTIFICATE
+	u.Update("Configuring SSL Certificate...")
+
+	if gc.SSLCert.Exists() {
+		u.Step(terminal.StatusOK, "Found existing SSL Certificate")
+	} else {
+		u.Update("Generating Google-managed SSL Certificate...")
+		_, err := gc.SSLCert.Create(rm.config.Domain)
+		if err != nil {
+			return nil, fmt.Errorf("failed to generate SSL Certificate: %s", err.Error())
+		}
+
+		u.Step(terminal.StatusOK, "Generated Google-managed SSL Certificate")
+	}
+
+	// PROVISION HTTPS PROXY
+	u.Update("Configuring HTTPS proxy...")
+
+	if gc.Proxy.Exists("https") {
+		u.Step("Found existing HTTPS proxy")
+	} else {
+		u.Update("Creating new HTTPS proxy...")
+		_, err := gc.Proxy.Create("https")
+		if err != nil {
+			return nil, fmt.Errorf("failed to create HTTPS proxy: %s", err.Error())
+		}
+
+		u.Step(terminal.StatusOK, "Created new HTTPS proxy")
+	}
+
+	// CREATE FORWARDING RULE
+	u.Update("Configuring forwarding rules...")
+
+	if gc.ForwardRule.Exists() {
+		u.Step("Found existing forwarding rule")
+	} else {
+		u.Update("Creating new forwarding rule...")
+		_, err := gc.ForwardRule.Create()
+		if err != nil {
+			return nil, fmt.Errorf("failed to create forwarding rule: %s", err.Error())
+		}
+
+		u.Step(terminal.StatusOK, "Created new forwarding rule")
+	}
 
 	return &Release{}, nil
 }
-
-//TODO: configure target https proxy
-// see - gcloud compute target-https-proxies create --help
-//TODO: configure forwarding rule to reserved ip and target proxy
