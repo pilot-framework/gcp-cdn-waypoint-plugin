24c4a64e8fa8b622cb8c4b18633945ab0ecfe59f
Vincent Bundage <vincentbundage@gmail.com>
2021-07-12 19:07:42 -0600

    feat: check for existing external ip and generate new ip

--- release/release.go
+++ release/release.go
@@ -6,6 +6,7 @@ import (
 	"errors"
 	"fmt"
 	"os/exec"
+	"strings"
 
 	"github.com/hashicorp/waypoint-plugin-sdk/terminal"
 	"github.com/pilot-framework/gcp-cdn-waypoint-plugin/platform"
@@ -41,44 +42,65 @@ func (rm *ReleaseManager) ReleaseFunc() interface{} {
 	return rm.release
 }
 
-// A BuildFunc does not have a strict signature, you can define the parameters
-// you need based on the Available parameters that the Waypoint SDK provides.
-// Waypoint will automatically inject parameters as specified
-// in the signature at run time.
-//
-// Available input parameters:
-// - context.Context
-// - *component.Source
-// - *component.JobInfo
-// - *component.DeploymentConfig
-// - *datadir.Project
-// - *datadir.App
-// - *datadir.Component
-// - hclog.Logger
-// - terminal.UI
-// - *component.LabelSet
-
-// In addition to default input parameters the platform.Deployment from the Deploy step
-// can also be injected.
-//
-// The output parameters for ReleaseFunc must be a Struct which can
-// be serialzied to Protocol Buffers binary format and an error.
-// This Output Value will be made available for other functions
-// as an input parameter.
-//
-// If an error is returned, Waypoint stops the execution flow and
-// returns an error to the user.
 func (rm *ReleaseManager) release(ctx context.Context, ui terminal.UI, target *platform.Deployment) (*Release, error) {
 	u := ui.Status()
 	defer u.Close()
 	u.Step("", "---Releasing to Cloud CDN---")
+	// res, err := GetStaticIP(target.Bucket, target.Project)
+	out, err := ListIPAddresses(target.Project)
+	if err != nil {
+		return nil, fmt.Errorf("failed to get listing of external IPs: %s", err.Error())
+	}
+
+	if strings.Contains(out, target.Bucket) {
+		u.Step(terminal.StatusOK, "Found existing external IP address")
+	} else {
+		u.Step("", "Generating external IP address")
+		_, err := GenerateIPAddress(target.Bucket, target.Project)
+		if err != nil {
+			return nil, fmt.Errorf("failed to get listing of external IPs: %s", err.Error())
+		}
+		u.Step(terminal.StatusOK, "External IP generated")
+	}
+
+	ipAddr, err := GetStaticIP(target.Bucket, target.Project)
+	if err != nil {
+		return nil, fmt.Errorf("failed to get external IP: %s", err.Error())
+	}
+
+	u.Step("", "ipaddress="+ipAddr)
 
 	return &Release{}, nil
 }
 
-func GetStaticIP(ipName string) (string, error) {
+func GetStaticIP(b, projID string) (string, error) {
+	ipName := b + "-ip"
 	var stdout, stderr bytes.Buffer
-	cmd := exec.Command("gcloud", "compute", "addresses", "describe", ipName, "--format=get(address)", "--global")
+	cmd := exec.Command("gcloud", "compute", "addresses", "describe", ipName, "--format=get(address)", "--global", "--project="+projID)
+	cmd.Stdout = &stdout
+	cmd.Stderr = &stderr
+	err := cmd.Run()
+	if err != nil {
+		return "", errors.New(string(bytes.TrimSpace(stderr.Bytes())))
+	}
+	return string(bytes.TrimSpace(stdout.Bytes())), nil
+}
+
+func ListIPAddresses(projID string) (string, error) {
+	var stdout, stderr bytes.Buffer
+	cmd := exec.Command("gcloud", "compute", "addresses", "list", "--project="+projID)
+	cmd.Stdout = &stdout
+	cmd.Stderr = &stderr
+	err := cmd.Run()
+	if err != nil {
+		return "", errors.New(string(bytes.TrimSpace(stderr.Bytes())))
+	}
+	return string(bytes.TrimSpace(stdout.Bytes())), nil
+}
+
+func GenerateIPAddress(b, projID string) (string, error) {
+	var stdout, stderr bytes.Buffer
+	cmd := exec.Command("gcloud", "compute", "addresses", "create", b+"-ip", "--network-tier=PREMIUM", "--ip-version=IPV4", "--global", "--project="+projID)
 	cmd.Stdout = &stdout
 	cmd.Stderr = &stderr
 	err := cmd.Run()
